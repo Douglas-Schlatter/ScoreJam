de8a176827e38d9985c38029b780bc68db980a92
Douglas-Schlatter <douglas.ardenghi18@gmail.com>
2022-08-13 04:19:45 -0300

    Added Spwaner and pathfinding

--- Assets/Scripts/BaseEnemyController.cs
+++ Assets/Scripts/BaseEnemyController.cs
@@ -7,19 +7,33 @@ public class BaseEnemyController : MonoBehaviour
     public int health = 2;
 
     public GameObject deathEffect;
-    public GameObject GameController;
+    public float lastHit = 0;
+    public  GameObject a;
+    public Pathfinding.AIDestinationSetter pf;
+
+
+
+    private void Awake()
+    {
+        pf = a.GetComponentInChildren<Pathfinding.AIDestinationSetter>();
+        pf.target = GameController.iCont.player.transform;
+    }
 
     public void TakeDamage(int damage)
     {
+  
+        lastHit = GameController.iCont.timer;
         health -= damage;
         if (health <= 0)
         {
             Die();
         }
+
     }
 
     void Die()
     {
+        GameController.iCont.sEnemies.Remove(this.gameObject);
         GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(effect, 1f);
         Destroy(gameObject);
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-
+    public static GameController iCont;// { get; private set; }
     public GameObject player;
+
+
+
+    //Time Related
+    public float timer = 0.0f;
+    public float lastSpwan = 0.0f;
+
+    //Spwan Related
+    public GameObject[] spawanLocations;
+    public GameObject[] enemies;
+    public List <GameObject> sEnemies;// spwaned enemies, at the moment in the scene
+    public int rEnem;
+    public int rSpwan;
+
+    private void Awake()
+    {
+        iCont = iCont is null ? this : iCont;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +33,26 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
 
+        timer += Time.deltaTime;
+        if (6 > sEnemies.Count) 
+        {
+            if ((timer - lastSpwan) > 2.0)
+            {
+                rSpwan = Random.Range(0, spawanLocations.Length);
+                rEnem = Random.Range(0, enemies.Length);
+                GameObject test = (GameObject)Instantiate(enemies[rEnem], spawanLocations[rSpwan].transform.position, spawanLocations[rSpwan].transform.rotation);
+                //st.TryGetComponent(Pathfinding.AIDestinationSetter)
+                sEnemies.Add(test);
+                lastSpwan = timer;
+            }
+        }
     }
 }

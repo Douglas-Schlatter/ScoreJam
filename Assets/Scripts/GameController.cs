9e2d7279655163198e8b7b2e8b0597b76a9c35b1
Caetano Luiz Aneris Dassi <CaitaXD@gmail.com>
2022-08-15 03:28:41 -0300

    Tryng to implement Score

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using LootLocker.Requests;
+using UnityEngine.UI;
+using TMPro;
 public class GameController : MonoBehaviour
 {
     public static GameController iCont { get; private set; }
@@ -15,11 +17,18 @@ public class GameController : MonoBehaviour
     public Sprite CoraçãoVazio;
     public PlayerController pc;
 
+    int LeaderBoardID = 5520;
+    public TMP_Text PlayersNames;
+    public TMP_Text PlayerScores;
+    public TMP_Text CurrentScore;
+    public TMP_InputField playerNameInputsField;    
+
     //score related
     public int score;
     public int multplier;
     public float testHit;
 
+    bool _nameSubmited = false;
     //Time Related
     public float timer = 0.0f;
     public float lastSpwan = 0.0f;
@@ -33,15 +42,17 @@ public class GameController : MonoBehaviour
     public int rEnem;
     public int rSpwan;
 
+    bool setuped = false;
+
     private void Awake()
     {
         iCont = iCont is null ? this : iCont;
         morto = false;
-        vida = pc.life;
     }
     // Start is called before the first frame update
     void Start()
     {
+        vida = pc.life;
     }
 
     // Update is called once per frame
@@ -56,15 +67,137 @@ public class GameController : MonoBehaviour
         else if (vida == 1)
         {
             Vida2.sprite = CoraçãoVazio;
-        }else if (vida == 0)
+        }
+        else if (vida <= 0)
         {
             morto = true;
-            SceneManager.LoadScene(4);
+            CurrentScore.text = "Your Score: " + score;
+            Camera.main.transform.position = new Vector3(1000,1000,1000);
+            if(!setuped)
+            {   
+                StartCoroutine(Setup());
+                setuped = true;
+            }
+            else if(_nameSubmited)
+            {
+                StartCoroutine(ScoreRoutine());
+            }
+            //TODO> LOAD CREDITS
         }
     }
+    public IEnumerator Setup()
+    {
+        CurrentScore.gameObject.SetActive(true);
+        playerNameInputsField.gameObject.SetActive(true);
+        yield return LoginRoutine();
+    }    
+    public IEnumerator ScoreRoutine()
+    {
+        CurrentScore.gameObject.SetActive(false);
+        playerNameInputsField.gameObject.SetActive(false);
+        PlayerScores.gameObject.SetActive(true);
+        PlayersNames.gameObject.SetActive(true);
+        yield return SubmitScoreRoutine();
+        yield return FechTopHighScoresRoutine();
+    }
+    public void SetPlayerName()
+    {
+        LootLockerSDKManager.SetPlayerName(playerNameInputsField.text,
+            response =>
+            {
+                if (response.success)
+                {
+                    Debug.Log("Player name set");
+                    _nameSubmited = true;
+                }
+                else
+                {
+                    Debug.Log($"Did not Set Player Name {response.Error}");
+                    _nameSubmited = true;
+                }
+            }
+        );
+    }
+    IEnumerator FechTopHighScoresRoutine()
+    {
+        bool done = false;
+        void responseCallback(LootLockerGetScoreListResponse response)
+        {
+            if (response.success)
+            {
+                string names = "Names\n";
+                string scores = "Scores\n";
 
+                LootLockerLeaderboardMember[] members = response.items;
+                for (var i = 0; i<members.Length; i++)
+                {
+                    names += members[i].rank + ". ";
+                    print(members[i].player.name);
+                    if (members[i].player.name != "")
+                    {
+                        names += members[i].player.name;
+                    }
+                    else
+                    {
+                        names += members[i].player.id;
+                    }
+                    scores += members[i].score + "\n";
+                    names += "\n";
+                }
+                done = true;
+                PlayersNames.text = names;
+                PlayerScores.text = scores;
+            }
+            else
+            {
+                Debug.Log("Error"  +  response.Error);
+                done = true;
+            }
+        }
+        LootLockerSDKManager.GetScoreListMain(LeaderBoardID, 10, 0, responseCallback);
+        yield return new WaitUntil(() => !done);
+    }
 
-
+    IEnumerator LoginRoutine()
+    {
+        bool done = false;
+        LootLockerSDKManager.StartGuestSession(resposne =>
+        {
+            if (resposne.success)
+            {
+                Debug.Log("Login Successful");
+                PlayerPrefs.SetString("PlayerID", $"{resposne.player_id}");
+                done = true;
+            }
+            else
+            {
+                Debug.Log("Login Failed");
+                done = true;
+            }
+        });
+        yield return new WaitWhile(() => !done);
+    }
+    public IEnumerator SubmitScoreRoutine()
+    {
+        bool done = false;
+        string playerID = PlayerPrefs.GetString("PlayerID");
+        LootLockerSDKManager.SubmitScore(playerID, score: score, leaderboardId: LeaderBoardID,
+            resposne =>
+            {
+                if (resposne.success)
+                {
+                    Debug.Log("Score Submitted");
+                    done = true;
+                }
+                else
+                {
+                    Debug.Log("Score Submission Failed" + resposne.Error);
+                    done = true;
+                }
+            }
+        );
+        yield return new WaitWhile(() => !done);
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -76,24 +209,24 @@ public class GameController : MonoBehaviour
         {
 
             case 1:
-                SpawnByTime(6);
-                break;
+            SpawnByTime(6);
+            break;
             case 2:
-                SpawnByTime(7);
-                break;
+            SpawnByTime(7);
+            break;
             case 4:
-                SpawnByTime(8);
-                break;
+            SpawnByTime(8);
+            break;
             case 6:
-                SpawnByTime(9);
-                break;
+            SpawnByTime(9);
+            break;
             case 10:
-                SpawnByTime(10);
-                break;
+            SpawnByTime(10);
+            break;
 
             default:
-                SpawnByTime(6);
-                break;
+            SpawnByTime(6);
+            break;
         }
         /*
          if (6 > sEnemies.Count) 
@@ -155,7 +288,7 @@ public class GameController : MonoBehaviour
 
         genMult();
         score += addScore * multplier;
-           
- 
+
+
     }
 }
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LootLocker.Requests;
+using TMPro;
+public class ScoreBoard : MonoBehaviour
+{
+
+    public int score = 0;
+
+
+    bool _nameSubmited = false;
+
+    private void Start()
+    {
+
+    }
+
+   
+
+}
